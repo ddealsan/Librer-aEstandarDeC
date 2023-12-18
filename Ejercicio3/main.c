cf835ae4f733ed608c2ca6817535d6cb5a9046c6
ddealsan <ddealsan@myuax.com>
2023-12-18 11:52:09 +0100

    Avanzamos

--- Ejercicio3/main.c
+++ Ejercicio3/main.c
@@ -1,1 +1,10 @@
 #include <stdio.h>
+
+int main() {
+    // Parte a - Abrir un archivo de texto para lectura
+    FILE *file = fopen("file.txt", "r");
+    if (file == NULL) {
+        printf("El archivo no se pudo abrir.\n");
+        return 1;
+    }
+
