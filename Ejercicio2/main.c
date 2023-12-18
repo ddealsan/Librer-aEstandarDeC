c0acfae92864f9bead6346c71405ea285ce77a96
ddealsan <ddealsan@myuax.com>
2023-12-18 11:49:37 +0100

    Avanzamos

--- Ejercicio2/main.c
+++ Ejercicio2/main.c
@@ -15,5 +15,7 @@ int main() {
     }
     // Parte b - Calcular la longitud de una cadena
     char str[] = "Hola mundo";
-
+    int length = strlen(str);
+    printf("La longitud de '%s' es: %d\n", str, length);
+    return 0;
     }
