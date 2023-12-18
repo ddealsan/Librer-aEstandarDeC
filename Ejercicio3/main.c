fbf3235977e93d1ae53c3db4abced8b3464d421b
ddealsan <ddealsan@myuax.com>
2023-12-18 11:52:20 +0100

    Avanzamos

--- Ejercicio3/main.c
+++ Ejercicio3/main.c
@@ -8,3 +8,12 @@ int main() {
         return 1;
     }
 
+// Cerrar el archivo al final de la lectura
+    fclose(file);
+
+    // Parte b - Escribir y leer en un archivo de texto
+    file = fopen("file.txt", "w");
+    if (file == NULL) {
+        printf("El archivo no se pudo abrir para escritura.\n");
+        return 1;
+    }
