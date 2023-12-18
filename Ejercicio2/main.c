ee32ff33cf50539bcae6bb2ebf556c3357120ee4
ddealsan <ddealsan@myuax.com>
2023-12-18 11:48:38 +0100

    Avanzamos

--- Ejercicio2/main.c
+++ Ejercicio2/main.c
@@ -5,3 +5,7 @@ int main() {
     // Parte a - Comparar dos cadenas de caracteres
     char str1[] = "Hola";
     char str2[] = "Mundo";
+
+    int comparison = strcmp(str1, str2);
+
+    if (comparison == 0) {
