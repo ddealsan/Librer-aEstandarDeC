9122e59f3abb87772d98ce565ff7265033b4013e
ddealsan <ddealsan@myuax.com>
2023-12-18 11:52:43 +0100

    Avanzamos

--- Ejercicio3/main.c
+++ Ejercicio3/main.c
@@ -18,3 +18,25 @@ int main() {
         return 1;
     }
 
+    fprintf(file, "Hola, mundo!\n");
+
+    // Cerrar el archivo después de escribir
+    fclose(file);
+
+    // Abrir de nuevo para leer
+    file = fopen("file.txt", "r");
+    if (file == NULL) {
+        printf("El archivo no se pudo abrir para lectura.\n");
+        return 1;
+    }
+
+    char buffer[50];
+    fscanf(file, "%s", buffer);
+
+    printf("Leído del archivo: %s\n", buffer);
+
+    // Cerrar el archivo después de leer
+    fclose(file);
+
+    return 0;
+}
